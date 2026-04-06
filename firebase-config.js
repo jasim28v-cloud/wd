9f4400bdea08e97a38cf0d660dc383614070dca0
jasim28v-cloud <jasim28v@gmail.com>
2026-04-06 14:22:52 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-// ==================== WAVE - Firebase Configuration ====================
+// ==================== ZORAK - Firebase Configuration ====================
 const firebaseConfig = {
     apiKey: "AIzaSyD436FkIwy_e9tK5aW1DVPfUpeGZHeedUk",
     authDomain: "zorak-e0a51.firebaseapp.com",
-    databaseURL: "https://zorak-e0a51-default-rtdb.firebaseio.com",
+    databaseURL: "https://zorak-e0a51-default-rtdb.firebaseio.com/",
     projectId: "zorak-e0a51",
     storageBucket: "zorak-e0a51.firebasestorage.app",
-    messagingSenderId: "34407692791",
-    appId: "1:34407692791:web:bdd5d72c9c840afb6416f2",
-    measurementId: "G-NQVTB8RBKW"
+    appId: "1:34407692791:web:bdd5d72c9c840afb6416f2"
 };
 
 // Initialize Firebase
@@ -21,13 +19,15 @@ const storage = firebase.storage();
 // Cloudinary
 const CLOUD_NAME = 'dnmpmysk6';
 const UPLOAD_PRESET = 'do_2gg';
-const AGORA_APP_ID_CALL = '4017f66ea15f4ce088e8d8993a072a5b';
+
+// Agora
+const AGORA_APP_ID = '4017f66ea15f4ce088e8d8993a072a5b';
 
 // Admin Account
-const ADMIN_EMAIL = 'jasim33v@gmail.com';
-const ADMIN_PASSWORD = 'xx2314xx';
+const ADMIN_EMAIL = 'jasim66v@gmail.com';
+const ADMIN_PASSWORD = 'dd2314dd';
 
 // Site Name
-const SITE_NAME = 'WAVE';
+const SITE_NAME = 'ZORAK';
 
-console.log('🌊 WAVE - Firebase, Cloudinary & Agora Ready');
+console.log('✅ ZORAK Ready');
